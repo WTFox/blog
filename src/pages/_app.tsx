3bc40fb200346aa35b5e99bf52ec00a9bdacebba
A. Fox <anthonyfox1988@gmail.com>
2022-03-14 20:04:47 -0700

    how do we feel about particles?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Fonts from "@/components/Fonts";
 import { AppProps } from "next/app";
 
 import Layout from "@/components/Layout";
+import Particles from "react-tsparticles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,42 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ChakraProvider resetCSS theme={theme}>
       <Fonts />
       <Layout>
+        <Particles
+          options={{
+            fpsLimit: 30,
+            particles: {
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "right",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                value: 50,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
