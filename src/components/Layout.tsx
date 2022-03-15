71579d3bfb07dfaea975ae1530e4687fe91616e7
A. Fox <anthonyfox1988@gmail.com>
2022-03-14 19:50:16 -0700

    adjust responsiveness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,19 +5,23 @@ import { Grid, GridItem } from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
   return (
-    <Grid templateColumns="repeat(8, 1fr)" gap={0}>
+    <Grid templateColumns="repeat(10, 1fr)" gap={0}>
+      <GridItem
+        colStart={{ base: 0, xl: 1 }}
+        colSpan={{ base: 0, xl: 1 }}
+      ></GridItem>
       <GridItem
         pt={"5em"}
-        colStart={{ base: 1, xl: 1 }}
-        colSpan={{ base: 8, xl: 3 }}
+        colStart={{ base: 1, xl: 2 }}
+        colSpan={{ base: 10, xl: 3 }}
         w="100%"
       >
         <Nav />
       </GridItem>
       <GridItem
         pt={"5em"}
-        colStart={{ base: 1, xl: 4 }}
-        colSpan={{ base: 8, xl: 4 }}
+        colStart={{ base: 1, xl: 5 }}
+        colSpan={{ base: 10, xl: 5 }}
         w="100%"
       >
         {children}
