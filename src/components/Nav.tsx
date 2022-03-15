5da0100e85d12e6bddf03aebae34957938d25d7d
A. Fox <anthonyfox1988@gmail.com>
2022-03-14 18:29:23 -0700

    Profile pic links home

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import { Box, Image, useColorModeValue } from "@chakra-ui/react";
+import Link from "next/link";
 
 import Section from "./Section";
 
@@ -7,24 +8,26 @@ const NavContent = () => {
   return (
     <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }} textAlign="center">
       <Section delay={0.1}>
-        <Box
-          borderColor={color}
-          borderWidth={10}
-          borderStyle="solid"
-          w="250px"
-          h="250px"
-          display="inline-block"
-          borderRadius="full"
-          overflow="hidden"
-        >
-          <Image
-            src="https://avatars.githubusercontent.com/u/2353242?v=4"
-            alt="A. Fox"
+        <Link href={"/"}>
+          <Box
+            borderColor={color}
+            borderWidth={10}
+            borderStyle="solid"
+            w="250px"
+            h="250px"
+            display="inline-block"
             borderRadius="full"
-            width="100%"
-            height="100%"
-          />
-        </Box>
+            overflow="hidden"
+          >
+            <Image
+              src="https://avatars.githubusercontent.com/u/2353242?v=4"
+              alt="A. Fox"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Link>
       </Section>
     </Box>
   );
