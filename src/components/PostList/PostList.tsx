a60846d118ee77ebd2443693c39beffcfe8823c0
A. Fox <anthonyfox1988@gmail.com>
2022-03-18 12:22:26 -0700

    remove comments

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -14,13 +14,7 @@ export const PostList = ({ posts }) => {
     <Stack px={{ base: 8, lg: 20 }}>
       <Section pb={5} delay={0.1}>
         <Heading id="Posts" size="lg">
-          <Text
-            display={"inline-block"}
-            // bgClip={"text"}
-            // bgGradient={"linear(to-l, #7928CA, #FF0080)"}
-          >
-            {"❯_"}
-          </Text>
+          <Text display={"inline-block"}>{"❯_"}</Text>
           {" writings"}
         </Heading>
         <Divider pt={5} size={"md"} />
