14af09f2a4b73b8893ff78324e7227f094efa787
A. Fox <anthonyfox1988@gmail.com>
2022-04-21 16:23:35 -0700

    More updates to about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,36 +1,83 @@
-import { Stack, Divider, Heading, Text } from "@chakra-ui/react";
+import {
+  Text,
+  Box,
+  Image,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react"
 
-import { MainLayout } from "@/components/Layouts";
-import { ReactElement } from "react";
-import Section from "@/components/Section";
+import { ReactElement } from "react"
+import Section from "@/components/Section"
+import { FullWidthLayout } from "@/components/Layouts"
+
+import me from "../../public/images/me.jpg"
+import bike from "../../public/images/bike.png"
+import { Container } from "@/components/Container"
+
+function getAge(dateString: string): string {
+  var today = new Date()
+  var birthDate = new Date(dateString)
+  var age = today.getFullYear() - birthDate.getFullYear()
+  var m = today.getMonth() - birthDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--
+  }
+  return age.toFixed(0)
+}
+
+const Sect = (children: any) => {
+  return <Section pb={5} delay={0.3} {...children} />
+}
 
 const About = () => {
-  return (
-    <Stack px={{ base: 8, lg: 20 }}>
-      <Section pb={5} delay={0.1}>
-        <Heading id="Posts" size="lg">
-          <Text display={"inline-block"}>{"❯_ about"}</Text>
-        </Heading>
-        <Divider pt={5} size={"md"} />
-      </Section>
+  const age = getAge("10-05-1988")
 
-      <Section pb={5} delay={0.3}>
-        <Text>
-          Anthony is an experienced speaker, tech blogger, and dedicated
-          software engineer. In the past, he’s participated in weekend
-          hack-a-thons, authored projects that made the front page of Hacker
-          News, and has been featured in programming newsletters such as Django
-          Weekly, and PyCoders Weekly. When Anthony isn’t glued to a glowing
-          rectangle, he spends time reading, exercising, and going on road-trips
-          with his wife, Paola, and aussie, Alto.
-        </Text>
-      </Section>
-    </Stack>
-  );
-};
+  const { colorMode } = useColorMode()
+  const borderColor = useColorModeValue("#7928CA", "#FF0080")
+
+  let borderProps: any
+  if (colorMode === "light") {
+    borderProps = { borderColor }
+  } else {
+    borderProps = { bgGradient: "linear(to-l, #7928CA, #FF0080)" }
+  }
+
+  return (
+    <Container maxW={"40rem"} px={{ base: 8, lg: 20 }}>
+      <Sect>
+        <Box
+          borderWidth={2}
+          w={"20rem"}
+          display="inline-block"
+          borderRadius="50"
+          overflow="hidden"
+          {...borderProps}
+        >
+          <Image src={me.src} alt="Picture of Anthony" />
+        </Box>
+      </Sect>
+
+      <Sect>
+        <Text>Hello, I&apos;m Anthony Fox.</Text>
+      </Sect>
+
+      <Sect>
+        I&apos;m {age} years old and I&apos;m currently a Principal Engineer for
+        Stratasan, a company focused on healthcare analytics.
+      </Sect>
+
+      <Sect>
+        I live in California with my wife, Paola, and my Australian Shepher,
+        Alto.
+      </Sect>
+
+      <Sect>You can follow me on Strava, if you&apos;d like.</Sect>
+    </Container>
+  )
+}
 
 About.getLayout = function getLayout(page: ReactElement) {
-  return <MainLayout>{page}</MainLayout>;
-};
+  return <FullWidthLayout>{page}</FullWidthLayout>
+}
 
-export default About;
+export default About
