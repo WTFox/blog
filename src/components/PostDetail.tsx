2f70b899cc0eb03c7d35171877d9b21586ad51c5
A. Fox <anthonyfox1988@gmail.com>
2022-04-22 11:52:58 -0700

    fix padding again

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -23,7 +23,7 @@ const Footer = ({ date }) => {
 }
 
 const PostDetail = (props: Props) => (
-  <Box px={"5rem"} textAlign={"justify"}>
+  <Box px={{ base: 0, xl: "16rem" }} textAlign={"justify"}>
     <Heading
       textAlign={"center"}
       bgGradient="linear(to-l, #7928CA, #FF0080)"
