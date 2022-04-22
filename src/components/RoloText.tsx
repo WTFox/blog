cbe2dd368b6b8b1ecf726a5ad24a894e5b7244e5
A. Fox <anthonyfox1988@gmail.com>
2022-04-21 20:36:00 -0700

    Add rotating text component

--- src/components/RoloText.tsx
+++ src/components/RoloText.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react"
 import { Text } from "@chakra-ui/react"
 
+import SiteConfig from "@/lib/config"
+
 const RoloText = ({ children }) => {
+  const subtitles: typeof SiteConfig["subtitles"] = children
   const [index, setIndex] = useState(0)
 
   useEffect(() => {
     setTimeout(() => {
-      setIndex((index + 1) % children.length)
+      setIndex((index + 1) % subtitles.length)
     }, 2500)
   })
 
-  return <Text as={"i"}>{children[index]}</Text>
+  return <Text as={"i"}>{subtitles[index]}</Text>
 }
 
 export default RoloText
