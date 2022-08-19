937adfb285589331ecbc69e07704cd5e12e99cd5
A. Fox <anthonyfox1988@gmail.com>
2022-08-19 15:00:43 -0700

    Add title to About

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,12 +16,14 @@ import Rainbowify from "@/components/Rainbowify"
 import me from "../../public/images/me.jpg"
 import bike from "../../public/images/bike.png"
 import SiteConfig from "@/lib/SiteConfig"
+import Head from "next/head"
 
 function getAge(): number {
-  var today = new Date()
-  var birthDate = new Date(1988, 10, 5)
-  var age = today.getFullYear() - birthDate.getFullYear()
-  var m = today.getMonth() - birthDate.getMonth()
+  const today = new Date()
+  const birthDate = new Date(1988, 10, 5)
+  const m = today.getMonth() - birthDate.getMonth()
+  let age = today.getFullYear() - birthDate.getFullYear()
+
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--
   }
@@ -47,87 +49,93 @@ const About = () => {
   const fontSize = "xl"
 
   return (
-    <VStack maxW={"40rem"} textAlign={"justify"}>
-      <Section pb={5} delay={0.3}>
-        <Box
-          borderWidth={2}
-          display="inline-block"
-          borderRadius="50"
-          overflow="hidden"
-          {...borderProps}
-        >
-          <Image src={me.src} alt="Picture of Anthony" />
-        </Box>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Text fontSize={fontSize}>Hello, I&apos;m Anthony Fox.</Text>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Text fontSize={fontSize}>
-          I&apos;m a <Rainbowify>{age}</Rainbowify> year old
-          <Rainbowify> Principal Engineer </Rainbowify>
-          for <Rainbowify>Stratasan</Rainbowify>, a company focused on
-          healthcare analytics.
-        </Text>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Text fontSize={fontSize}>
-          Learning is my passion and I prefer to learn by reading. Reading has
-          always helped me keep imposter syndrome at bay, but what good is
-          knowledge if you don&apos;t share it?
-        </Text>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Text fontSize={fontSize}>
-          My days are usually spent pairing with others on my team, sharing best
-          practices, and trying to keep up with the ever-changing landscape that
-          is software engineering. I&apos;ve also spoken at many meetups and a
-          few conferences, too.
-        </Text>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Text fontSize={fontSize}>
-          I live in California with my wife, Paola, and my Australian Shepherd,
-          Alto. When I&apos;m not programming, reading, or spending time with my
-          wife, I&apos;m on my bike.
-        </Text>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Box
-          borderWidth={2}
-          display="inline-block"
-          borderRadius="50"
-          overflow="hidden"
-          {...borderProps}
-        >
-          <Image src={bike.src} alt="Picture of Anthony's road bike." />
-        </Box>
-      </Section>
-
-      <Section pb={5} delay={0.3}>
-        <Text fontSize={fontSize}>
-          You can follow me on{" "}
-          <Link
-            textDecoration={"underline"}
-            isExternal
-            href={"https://strava.com/athletes/wtfox"}
+    <div>
+      <Head>
+        <title>{"About me!"}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <VStack maxW={"40rem"} textAlign={"justify"}>
+        <Section pb={5} delay={0.3}>
+          <Box
+            borderWidth={2}
+            display="inline-block"
+            borderRadius="50"
+            overflow="hidden"
+            {...borderProps}
           >
-            <Rainbowify>Strava</Rainbowify>
-          </Link>
-          , if you&apos;d like. I&apos;m more active during the summer. ;)
-        </Text>
-      </Section>
+            <Image src={me.src} alt="Picture of Anthony" />
+          </Box>
+        </Section>
 
-      <Section py={10} delay={0.3}>
-        {" "}
-      </Section>
-    </VStack>
+        <Section pb={5} delay={0.3}>
+          <Text fontSize={fontSize}>Hello, I&apos;m Anthony Fox.</Text>
+        </Section>
+
+        <Section pb={5} delay={0.3}>
+          <Text fontSize={fontSize}>
+            I&apos;m a <Rainbowify>{age}</Rainbowify> year old
+            <Rainbowify> Principal Engineer </Rainbowify>
+            for <Rainbowify>Stratasan</Rainbowify>, a company focused on
+            healthcare analytics.
+          </Text>
+        </Section>
+
+        <Section pb={5} delay={0.3}>
+          <Text fontSize={fontSize}>
+            Learning is my passion and I prefer to learn by reading. Reading has
+            always helped me keep imposter syndrome at bay, but what good is
+            knowledge if you don&apos;t share it?
+          </Text>
+        </Section>
+
+        <Section pb={5} delay={0.3}>
+          <Text fontSize={fontSize}>
+            My days are usually spent pairing with others on my team, sharing
+            best practices, and trying to keep up with the ever-changing
+            landscape that is software engineering. I&apos;ve also spoken at
+            many meetups and a few conferences, too.
+          </Text>
+        </Section>
+
+        <Section pb={5} delay={0.3}>
+          <Text fontSize={fontSize}>
+            I live in California with my wife, Paola, and my Australian
+            Shepherd, Alto. When I&apos;m not programming, reading, or spending
+            time with my wife, I&apos;m on my bike.
+          </Text>
+        </Section>
+
+        <Section pb={5} delay={0.3}>
+          <Box
+            borderWidth={2}
+            display="inline-block"
+            borderRadius="50"
+            overflow="hidden"
+            {...borderProps}
+          >
+            <Image src={bike.src} alt="Picture of Anthony's road bike." />
+          </Box>
+        </Section>
+
+        <Section pb={5} delay={0.3}>
+          <Text fontSize={fontSize}>
+            You can follow me on{" "}
+            <Link
+              textDecoration={"underline"}
+              isExternal
+              href={"https://strava.com/athletes/wtfox"}
+            >
+              <Rainbowify>Strava</Rainbowify>
+            </Link>
+            , if you&apos;d like. I&apos;m more active during the summer. ;)
+          </Text>
+        </Section>
+
+        <Section py={10} delay={0.3}>
+          {" "}
+        </Section>
+      </VStack>
+    </div>
   )
 }
 
