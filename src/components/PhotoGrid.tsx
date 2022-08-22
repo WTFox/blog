619cc4189c16b9dc370f6424b80d1122d28ad8ac
A. Fox <anthonyfox1988@gmail.com>
2022-08-22 15:25:43 -0700

    Add padding around photogrids

--- src/components/PhotoGrid.tsx
+++ src/components/PhotoGrid.tsx
@@ -1,31 +1,33 @@
 import path from "path"
 import Image from "next/image"
-import { SimpleGrid, Link as ChakraLink } from "@chakra-ui/react"
+import { Box, SimpleGrid, Link as ChakraLink } from "@chakra-ui/react"
 import Link from "next/link"
 
 const PhotoGrid = ({ slug, images }) => {
   return (
-    <SimpleGrid minChildWidth={"150px"} spacing={1}>
-      {images.map((image: string, index: number) => {
-        const alt = path.basename(image, path.extname(image))
-        const key = `${alt}-${index}`
-        const srcLink = require(`../../content/${slug}/${image}`).default
+    <Box py={5}>
+      <SimpleGrid minChildWidth={"150px"} spacing={5}>
+        {images.map((image: string, index: number) => {
+          const alt = path.basename(image, path.extname(image))
+          const key = `${alt}-${index}`
+          const srcLink = require(`../../content/${slug}/${image}`).default
 
-        return (
-          <Link key={key} href={srcLink.src} passHref>
-            <ChakraLink key={key} isExternal>
-              <Image
-                key={key}
-                loading="eager"
-                src={srcLink}
-                alt={alt}
-                layout={"responsive"}
-              />
-            </ChakraLink>
-          </Link>
-        )
-      })}
-    </SimpleGrid>
+          return (
+            <Link key={key} href={srcLink.src} passHref>
+              <ChakraLink key={key} isExternal>
+                <Image
+                  key={key}
+                  loading="eager"
+                  src={srcLink}
+                  alt={alt}
+                  layout={"responsive"}
+                />
+              </ChakraLink>
+            </Link>
+          )
+        })}
+      </SimpleGrid>
+    </Box>
   )
 }
 
