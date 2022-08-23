7831e6604bc2e34414b493dadd82ed5625e73671
A. Fox <anthonyfox1988@gmail.com>
2022-08-22 21:13:51 -0700

    move to single file

--- api/index.go
+++ api/index.go
@@ -9,3 +9,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	result := addTwoIntegersReturnString(1, 2)
 	fmt.Fprintf(w, "<h1>"+result+"</h1>")
 }
+
+func addTwoIntegersReturnString(a int, b int) string {
+	return fmt.Sprintf("%d", a+b)
+}
--- api/utils.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-import "fmt"
-
-// addTwoIntegersReturnString takes two integers and returns the sum as a string
-func addTwoIntegersReturnString(a int, b int) string {
-	return fmt.Sprintf("%d", a+b)
-}
