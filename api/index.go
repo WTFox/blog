7e953fdf199071430a0b61c7fede78442432d225
A. Fox <anthonyfox1988@gmail.com>
2022-08-22 21:06:17 -0700

    are you happy with this?

--- api/index.go
+++ api/index.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	result := AddTwoIntegersReturnString(1, 2)
+	result := addTwoIntegersReturnString(1, 2)
 	fmt.Fprintf(w, "<h1>"+result+"</h1>")
 }
--- api/utils.go
+++ api/utils.go
@@ -2,7 +2,7 @@ package handler
 
 import "fmt"
 
-// AddTwoIntegersReturnString takes two integers and returns the sum as a string
-func AddTwoIntegersReturnString(a int, b int) string {
+// addTwoIntegersReturnString takes two integers and returns the sum as a string
+func addTwoIntegersReturnString(a int, b int) string {
 	return fmt.Sprintf("%d", a+b)
 }
