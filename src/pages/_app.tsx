5913887c8f50ccd1e6e28ea6e9c9a0a22bca5f05
A. Fox <anthonyfox1988@gmail.com>
2023-09-21 13:57:05 -0700

    import styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,22 @@
-import "focus-visible/dist/focus-visible";
+import "focus-visible/dist/focus-visible"
 
-import type { ReactElement, ReactNode } from "react";
+import type { ReactElement, ReactNode } from "react"
 
-import type { AppProps } from "next/app";
-import type { NextPage } from "next";
+import type { AppProps } from "next/app"
+import type { NextPage } from "next"
+
+import "../styles/global.css"
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
+  Component: NextPageWithLayout
+}
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
+  const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(<Component {...pageProps} />)
 }
