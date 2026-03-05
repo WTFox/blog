2d6a642f009064d16ddf5e98178c46a72dacc9e0
A. Fox <anthonyfox1988@gmail.com>
2026-03-05 11:12:18 -0800

    fixes

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,45 +2,45 @@ import { ThemeConfig, extendTheme, theme as baseTheme } from "@chakra-ui/react"
 import { activeTheme } from "@/lib/themes"
 
 const fonts = {
-  heading: "DM Sans",
-  body: "DM Sans",
-  mono: "JetBrains Mono",
+    heading: "DM Sans",
+    body: "DM Sans",
+    mono: "JetBrains Mono",
 }
 
 const breakpoints = {
-  sm: "40em",
-  md: "52em",
-  lg: "64em",
-  xl: "80em",
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    xl: "80em",
 }
 
 const colors = {
-  primaryFontColor: {
-    lightMode: baseTheme.colors.gray["700"],
-    darkMode: baseTheme.colors.gray["200"],
-  },
+    primaryFontColor: {
+        lightMode: baseTheme.colors.gray["700"],
+        darkMode: baseTheme.colors.gray["200"],
+    },
 }
 
 const theme: ThemeConfig = extendTheme({
-  initialColorMode: "light",
-  useSystemColorMode: false,
-  fonts,
-  colors,
-  breakpoints,
-  styles: {
-    global: (props) => ({
-      "html, body": {
-        fontFamily: "DM Sans",
-        fontSize: "18px",
-        bg: props.colorMode === "dark" ? activeTheme.dark.bg : "white",
-      },
-      "pre, code": {
-        fontFamily: "JetBrains Mono, monospace",
-        fontFeatureSettings:
-          '"calt" 1, "clig" 1, "liga" 1, "ss02" 1,  "cv03" 1, "cv04" 1, "cv06" 1, "cv07" 1, "cv11" 1, "cv14" 1, "cv15" 1, "cv16" 1, "cv17" 1',
-      },
-    }),
-  },
+    initialColorMode: "light",
+    useSystemColorMode: true,
+    fonts,
+    colors,
+    breakpoints,
+    styles: {
+        global: (props) => ({
+            "html, body": {
+                fontFamily: "DM Sans",
+                fontSize: "18px",
+                bg: props.colorMode === "dark" ? activeTheme.dark.bg : "white",
+            },
+            "pre, code": {
+                fontFamily: "JetBrains Mono, monospace",
+                fontFeatureSettings:
+                    '"calt" 1, "clig" 1, "liga" 1, "ss02" 1,  "cv03" 1, "cv04" 1, "cv06" 1, "cv07" 1, "cv11" 1, "cv14" 1, "cv15" 1, "cv16" 1, "cv17" 1',
+            },
+        }),
+    },
 })
 
 export default theme
